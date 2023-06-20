c52b25af0bd2a59dcb4801c311084dedc65bb70b
Sidney Pauly <sidney.pauly@venios.de>
2023-06-20 17:37:03 +0100

    Added packet based protocol

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,83 @@
 #include "tempanalog.h"
 #include "myservo.h"
 
+#include <serial.hpp>
+
 MyServo myServo;
 AnalogThemp analogTempSensor;
 HumTemp humTempSensor;
 AbstractSensor* arr[2];
 
 
-void setup() {
+
+
+// void setup() {
+//   Serial.begin(9600);
+//   myServo.init();
+
+//   arr[0] = &analogTempSensor;
+//   arr[1] = &humTempSensor;
+
+//   analogTempSensor.init();
+//   humTempSensor.init();
+
+//   analogTempSensor.listen();
+// }
+
+// char message;
+
+// void input() {
+//   while(Serial.available()) {
+//       char c = Serial.read();
+
+//         if (c == 48 || c == 49)
+//           message = c;
+//         else if (c == 13) {
+//           if (message == 48) {
+//             myServo.open();
+//             Serial.write("OPEN!!!\n");
+//           }
+//           else if (message == 49){
+//             myServo.close();
+//             Serial.write("CLOSE!!!\n");
+//           }
+//           message = 50;
+//         }
+//   }
+// }
+
+// void loop() {
+//   for  (auto sensor : arr) {
+//     sensor->listen();
+//     sensor->loop();
+//     input();
+//     delay(1000);
+//   }
+// }
+
+void onPacket(RSSPacket &packet){
+
+}
+
+void setup(){
+
   Serial.begin(9600);
-  myServo.init();
 
-  arr[0] = &analogTempSensor;
-  arr[1] = &humTempSensor;
 
-  analogTempSensor.init();
-  humTempSensor.init();
+  startHardwareSerial(onPacket);
 
-  analogTempSensor.listen();
 }
 
-char message;
+void loop(){
 
-void input() {
-  while(Serial.available()) {
-      char c = Serial.read();
+  runRead();
 
-        if (c == 48 || c == 49)
-          message = c;
-        else if (c == 13) {
-          if (message == 48) {
-            myServo.open();
-            Serial.write("OPEN!!!\n");
-          }
-          else if (message == 49){
-            myServo.close();
-            Serial.write("CLOSE!!!\n");
-          }
-          message = 50;
-        }
-  }
+  RSSPacket packet;
+
+  packet.command = 0x22;
+  packet.payloadLength = 0;
+
+  delay(1000);
+
+  write(packet);
 }
-
-void loop() {
-  for  (auto sensor : arr) {
-    sensor->listen();
-    sensor->loop();
-    input();
-    delay(1000);
-  }
-}
-
-
-
-
